00686c4e27939484bee42a2971a98e9ddd7dfcc6
felix menard <info@iplanwebsites.com git config --global user.email info@iplanwebsites.com>
2011-06-07 00:10:03 +1000

    Created a basic JS (sammy) prototype that parses JSON and display back elements in a template. Pseudo-coded some routines in the Ruby file.

--- hello.rb
+++ hello.rb
@@ -12,6 +12,13 @@ require 'digest/md5'
 require "sinatra-authentication"
 
 
+# snapshot images using yotta webservices:
+# http://www.yottasnap.com/FAQ.aspx#SnapshotRequest
+# OUR api key: 32bca3bd-44df-4c04-a093-bd236912fcca
+# mode = FixedWidthAndHeightAutoCrop 
+# * we only have 5000 free snapshots for now.
+
+
 # authentification using sinatra-auth:
 # https://github.com/maxjustus/sinatra-authentication
 
@@ -27,6 +34,26 @@ get '/' do
   "Hello World! <a href='/lol'>Laugh page</a>"
 end
 
+get '/add-bookmark' do
+ # When adding a new bookmark:
+    # Clean the domain name
+    # If  already indexed, bump the fav counter, else,:
+      # CURL the homepage
+      # parse title, meta, and keywords (just strip-tags the whole page for now)
+      # Fetch the thumbnail URL at YottaSnap. (maybe wait until there's 2-3 bookmarkers to avoid caching spam domain images?)
+    
+      # we can then call robots-scripts after.
+        # check if google-ads are presents on page
+        # check if FB is there too
+        # check if there's an news feed (rss)?
+        # check PR? - probably too costly, and not very usefull anyway...
+        
+      
+end
+
+
+
+
 get '/lol' do
   data = [["hello", "coucou"],'hi',"hallo", "hehe", 123, 10-4] 
   JSONP data # JSONP is an alias for jsonp method, will return JSON string to browser.
@@ -37,8 +64,6 @@ get '/profile' do
   data = [["hello", "coucou"],'hi',"hallo", "hehe", 123, 10-4] 
   
   
-
-
   # find out the user's gravatar URL
   email_address = "info@iplanwebsites.com"
   hash = Digest::MD5.hexdigest(email_address)
--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,95 @@
-/* Author: 
+/* 
 
 */
 
+function throttle(fn, delay) {
+  var timer = null;
+  return function () {
+    var context = this, args = arguments;
+    clearTimeout(timer);
+    timer = setTimeout(function () {
+      fn.apply(context, args);
+    }, delay);
+  };
+}
 
 
 
+function cleanDomain(u){
+    s = u;
+    s = s.replace('http://www.', '');
+    s = s.replace('http://', '');
+    s = s.substr(0, s.indexOf('/'));
+    return s;
+    
+}
+
+
+$(document).ready(function() {
+    
+
+// initialize the application
+    sammy = Sammy('#main', function() {
+  // include a plugin
+  //this.use('Mustache');
+  this.use('Template');
+  
+  
+    this.get('#/', function(context) {
+            $.ajax({
+              url: 'js/delicious.json',
+              dataType: 'json',
+              success: function(items) {
+                  //alert('yay');
+                $.each(items, function(i, item) {
+                  context.log(item.d, '-', item.t, '-', item.u);
+                  //context.partial('templates/link.template');
+                  context.render('templates/link.template', {item: item})
+                                 .appendTo(context.$element());
+                                 
+                });
+              }
+            });
+          });
+  
+          
+    this.get('#/settings', function() {  
+               alert('settings');
+               $('#settings').addClass('show');
+
+           });
+  
+    this.post('#/search', function(context) { 
+        var key = this.params['q'];
+        console.log('searching '+key);
+        this.trigger('filterResults');
+        return false;
+      });
+      
+    this.post('#/cart', function(context) {
+        this.log("I'm in a post route. Add me to your cart");
+    });
+            
+      
+    
+});// end sammy defs
+
+
+    // start the application
+
+    sammy.run('#/');
+    
+    
+    
+    
+    //bind an hashchange on the search-field change.
+    $('#q').keypress(throttle(function (event) {
+        //update search feed.
+        this.trigger('filterResults');
+    }, 250));
+    
+    
+});//document ready...
 
 
 
