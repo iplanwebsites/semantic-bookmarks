d312f33a8d952bdedcb1054bb4f73d1957668541
felix menard <info@iplanwebsites.com>
2011-06-10 10:21:41 +1000

    improved the JS front-end prototype

--- public/js/script.js
+++ public/js/script.js
@@ -26,15 +26,57 @@ function cleanDomain(u){
     
 }
 
+function isUrl(s) {
+	//This function has to be more liberal,
+	// we want to let simple url entry work as well
+	// ie. facebook.com, bit.ly, www.google.com, etc)
+	var domain =/^([a-z0-9]([-a-z0-9]*[a-z0-9])?\\.)+((a[cdefgilmnoqrstuwxz]|aero|arpa)|(b[abdefghijmnorstvwyz]|biz)|(c[acdfghiklmnorsuvxyz]|cat|com|coop)|d[ejkmoz]|(e[ceghrstu]|edu)|f[ijkmor]|(g[abdefghilmnpqrstuwy]|gov)|h[kmnrtu]|(i[delmnoqrst]|info|int)|(j[emop]|jobs)|k[eghimnprwyz]|l[abcikrstuvy]|(m[acdghklmnopqrstuvwxyz]|mil|mobi|museum)|(n[acefgilopruz]|name|net)|(om|org)|(p[aefghklmnrstwy]|pro)|qa|r[eouw]|s[abcdeghijklmnortvyz]|(t[cdfghjklmnoprtvwz]|travel)|u[agkmsyz]|v[aceginu]|w[fs]|y[etu]|z[amw])$/i
+	var fullUrl = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+	if(fullUrl.test(s) || domain.test(s)){
+			return true
+	}else{
+		return false;
+	}
+}
+
 
 $(document).ready(function() {
     
+
+	$('#search_form').submit(function(event) {
+	event.preventDefault(); //so the form doesn't submit.
+	//var self = this;
+	;
+	str = $('#q').val();
+	if(isUrl(str)){
+		// addUrl(cleanDomain(str));
+		// we should  redirect to "#!/add/domain.com" so sammy.js routes can take over?
+	}else{ //if not a valid url it's either keywords filtering, or an incomplete Url...
+		// do a regular search.
+		// we update the hash only on submit...
+		// on keypress, we don't (it would really pollute the history stack...)
+
+	}
+
+	});
+	
+	
+	
+	/*
+	On keypress,
+		we can't add a URL directly once we detect something that looks like an URL.
+		so we rather just "show" to the user that we recorganized this as a link:
+		We add the class "is_url" to the text-field (so the text now appears blue+underlined)
+	
+	*/
     
     
     /*
     TODO: ovveride the form submit function in JS.
     Posting to a sammy function clear the fields, trigger a page refresh, etc...
     (not smooth now... )
+
+
     
     */
     
@@ -50,7 +92,7 @@ $(document).ready(function() {
   
     this.get('#/', function(context) {
             $.ajax({
-              url: 'js/delicious.json',
+              url: 'js/delicious.json', //won't work locally, be sure to run this unsing test server...
               dataType: 'json',
               success: function(items) {
                   //alert('yay');
@@ -67,7 +109,6 @@ $(document).ready(function() {
   
           
     this.get('#/settings', function() {  
-               alert('settings');
                $('#settings').addClass('show');
 
            });
@@ -95,11 +136,25 @@ $(document).ready(function() {
     
     
     
-    //bind an hashchange on the search-field change.
-    $('#q').keypress(throttle(function (event) {
+    //bind an hashchange on the search-field change?
+		
+		$('#q').bind('keypress keyup change focus click', function() {
+			//check if it's a URL...
+			//change style accordingly...
+			var str = $(this).val();
+			if( isUrl( str ) ) {
+				$(this).parent().addClass('is_url'); //we attach a special class to the form element
+			}else{
+				$(this).parent().removeClass('is_url');
+			}
+	
+			//this is the part we don't want to execute too often
+			throttle(function (event) {
         //update search feed.
         this.trigger('filterResults');
-    }, 250));
+    	}, 250);
+
+});
     
     
 });//document ready...
