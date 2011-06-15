e223c22441140799de32dbe39784126734b90632
Eduardo Lopez <eduardo.biagi@gmail.com>
2011-06-14 20:49:37 -0500

    Organize delicious spec
    Take advantage of describe, context and include_context to organize
    the specs.
    
    This should produce a very readable spec when run with the '--format'
    option set to 'documentation':
    
    rspec spec/delicious_spec.rb --format documentation

--- spec/delicious_spec.rb
+++ spec/delicious_spec.rb
@@ -1,81 +1,118 @@
 require_relative 'spec_helper'
-
-delicious_url = 'http://feeds.delicious.com/v2/json'
-
-delicious_json = %{[{"u":"http:\/\/vimeo.com\/24302498","d":"29 WAYS TO STAY CREATIVE on Vimeo","t":["video","creative","design","inspiration","creativity","via:packrati.us","criatividade","art","creatividad","dise\u00f1o"],"dt":"2011-06-04T03:36:20Z"},{"u":"http:\/\/scribblinlenore.livejournal.com\/559093.html","d":"In Plain Sight","t":["x-men","erik\/charles","charles\/erik","nc-17","author:scribblinlenore","fanfic","fic","au","scribblinlenore","xmen"],"dt":"2011-06-13T02:52:26Z"}]}
+require_relative 'delicious_context.rb'
 
 describe 'The Delicious App' do
+  include_context 'delicious context'
 
-  it 'should respond to /' do
-    get '/'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should match(/text\/html/)
-    last_response.should match(/Discover interesting websites/)
+  describe '#/ - homepage' do
+    before do
+      get '/'
+      @last_response = last_response
+    end
+
+    it { @last_response.should be_ok }
+
+    it 'includes "Discover interesting websites" in the body' do
+      @last_response.body.should include('Discover interesting websites')
+    end
+
+    it 'has a text/html content type' do
+      @last_response.headers['Content-Type'].should include('text/html')
+    end
   end
 
-  it 'should fetch hotlist bookmarks from delicious' do
-    uri = URI.parse(delicious_url)
+  describe '#/api/v1/delicious/feed - hotlist bookmarks' do
+    context 'when count is not specified' do
+      before do
+        mock_get_json URI.parse(@delicious_url)
+        get '/api/v1/delicious/feed'
+        @last_response = last_response
+      end
 
-    mock_req = double('http request')
-    mock_req.should_receive(:body) { delicious_json }
-    Net::HTTP.should_receive(:get_response).with(uri) { mock_req }
+      it { @last_response.should be_ok }
 
-    get '/api/v1/delicious/feed'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/json'
-    last_response.body.should == delicious_json
-    JSON.parse(last_response.body).length.should == 2
+      it 'has an application/json content type' do
+        @last_response.headers['Content-Type'].should == 'application/json'
+      end
+
+      it 'returns json from delicious' do
+        @last_response.body.should == delicious_json
+      end
+
+      it 'returns two bookmarks' do
+        JSON.parse(@last_response.body).length.should == 2
+      end
+    end
+
+    context 'when count is a number' do
+      before do
+        mock_get_json URI.parse("#{@delicious_url}?count=50")
+        get '/api/v1/delicious/feed?count=50'
+        @last_response = last_response
+      end
+
+      it 'adds a count parameter to the delicious request' do
+        @last_response.should be_ok
+      end
+    end
+
+    context 'when count is a negative number' do
+      before do
+        mock_get_json URI.parse("#{@delicious_url}")
+        get '/api/v1/delicious/feed?count=-100'
+        @last_response = last_response
+      end
+
+      it 'ignores the count parameter in the delicious request' do
+        @last_response.should be_ok
+      end
+    end
   end
 
-  it 'should limit the number of hotlist bookmark results' do
-    uri = URI.parse("#{delicious_url}?count=50")
+  describe '#/api/v1/delicious/feeds/recent - recent bookmarks' do
+    context 'when count is not specified' do
+      before do
+        mock_get_json URI.parse("#{@delicious_url}/recent")
+        get '/api/v1/delicious/feed/recent'
+        @last_response = last_response
+      end
 
-    mock_req = double('http request')
-    mock_req.should_receive(:body) { delicious_json }
-    Net::HTTP.should_receive(:get_response).with(uri) { mock_req }
+      it { @last_response.should be_ok }
 
-    get '/api/v1/delicious/feed?count=50'
-    last_response.should be_ok
+      it 'has an application/json content type' do
+        @last_response.headers['Content-Type'].should == 'application/json'
+      end
+
+      it 'returns json from delicious' do
+        @last_response.body.should == delicious_json
+      end
+
+      it 'returns two bookmarks' do
+        JSON.parse(@last_response.body).length.should == 2
+      end
+    end
+
+    context 'when count is a number' do
+      before do
+        mock_get_json URI.parse("#{@delicious_url}/recent?count=50")
+        get '/api/v1/delicious/feed/recent?count=50'
+        @last_response = last_response
+      end
+
+      it 'adds a count parameter to the delicious request' do
+        @last_response.should be_ok
+      end
+    end
   end
 
-  it "should ignore the count param if it's not a positive integer" do
-    uri = URI.parse("#{delicious_url}")
+  context 'when page cannot be found' do
+    before do
+      get '/404'
+      @last_response = last_response
+    end
 
-    mock_req = double('http request')
-    mock_req.should_receive(:body) { delicious_json }
-    Net::HTTP.should_receive(:get_response).with(uri) { mock_req }
-
-    get '/api/v1/delicious/feed?count=-100'
-    last_response.should be_ok
-  end
-
-  it 'should fetch recent bookmarks from delicious' do
-    uri = URI.parse("#{delicious_url}/recent")
-
-    mock_req = double('http request')
-    mock_req.should_receive(:body) { delicious_json }
-    Net::HTTP.should_receive(:get_response).with(uri) { mock_req }
-
-    get '/api/v1/delicious/feed/recent'
-    last_response.should be_ok
-    last_response.headers['Content-Type'].should == 'application/json'
-    last_response.body.should == delicious_json
-    JSON.parse(last_response.body).length.should == 2
-  end
-
-  it 'should limit the number of recent bookmark results' do
-    uri = URI.parse("#{delicious_url}/recent?count=50")
-
-    mock_req = double('http request')
-    mock_req.should_receive(:body) { delicious_json }
-    Net::HTTP.should_receive(:get_response).with(uri) { mock_req }
-
-    get '/api/v1/delicious/feed/recent?count=50'
-    last_response.should be_ok
-  end
-
-  it 'should return 404 when page cannot be found' do
-    get '/404'
-    last_response.status.should == 404
+    it 'returns 404' do
+      last_response.status.should == 404
+    end
   end
 end
