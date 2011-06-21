ad6774afa652d1ea09afb3f12fd27e2824d1c292
Eduardo Lopez <eduardo.lopez@jwmsolutions.com>
2011-06-20 19:27:16 -0500

    Fix failing specs
    Not sure why this started failing when the sinatra-authentication gem
    was added.

--- spec/delicious_context.rb
+++ spec/delicious_context.rb
@@ -2,7 +2,7 @@ shared_context "delicious context", :a => :b do
 
   before { @delicious_url = 'http://feeds.delicious.com/v2/json' }
 
-  let(:delicious_json) { %{[{"u":"http:\/\/vimeo.com\/24302498","d":"29 WAYS TO STAY CREATIVE on Vimeo","t":["video","creative","design","inspiration","creativity","via:packrati.us","criatividade","art","creatividad","dise\u00f1o"],"dt":"2011-06-04T03:36:20Z"},{"u":"http:\/\/scribblinlenore.livejournal.com\/559093.html","d":"In Plain Sight","t":["x-men","erik\/charles","charles\/erik","nc-17","author:scribblinlenore","fanfic","fic","au","scribblinlenore","xmen"],"dt":"2011-06-13T02:52:26Z"}]} }
+  let(:delicious_json) { %{[{"u":"http:\/\/vimeo.com\/24302498","d":"29 WAYS TO STAY CREATIVE on Vimeo","t":["video","creative","design","inspiration","creativity","via:packrati.us","criatividade","art","creatividad","dise\\u00f1o"],"dt":"2011-06-04T03:36:20Z"},{"u":"http:\/\/scribblinlenore.livejournal.com\/559093.html","d":"In Plain Sight","t":["x-men","erik\/charles","charles\/erik","nc-17","author:scribblinlenore","fanfic","fic","au","scribblinlenore","xmen"],"dt":"2011-06-13T02:52:26Z"}]} }
 
   def mock_get_json(url)
     mock_req = double('http request')
